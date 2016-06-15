6d377b1854307dfa187f45d82eaad2d078143ecf
M.C. van den Boogaart <scola@kvdb.name>
2016-06-15 11:45:42 +0200

    Add query formatter

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import * as url from 'url';
 import * as query from 'querystring';
 import parseHeader from './src/parse-header';
 
+const formatQuery = query.stringify;
 const parseUrl = url.parse;
 const parseQuery = query.parse;
 
 export {
+  formatQuery,
   parseHeader,
   parseUrl,
   parseQuery
